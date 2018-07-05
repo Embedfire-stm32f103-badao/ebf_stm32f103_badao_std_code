0404437071b7da01196d3bad9e969d87b9c63a9b
flyleaf <flyleaf91@163.com>
2018-07-05 16:53:23 +0800

    修改前的暂存

--- MP3播放器—VS1053/1.ISO-V3_VS1053裸机例程/Project/RVMDK（uv5）/RTE/_VS1053_MusicPlayer/RTE_Components.h
+++ MP3播放器—VS1053/1.ISO-V3_VS1053裸机例程/Project/RVMDK（uv5）/RTE/_VS1053_MusicPlayer/RTE_Components.h
@@ -4,11 +4,17 @@
  *      *** Do not modify ! ***
  *
  * Project: 'iSO-STM32' 
- * Target:  'GAME' 
+ * Target:  'VS1053_MusicPlayer' 
  */
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
 
 
 #endif /* RTE_COMPONENTS_H */
